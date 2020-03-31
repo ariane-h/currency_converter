b2768629dffd54d0fc79c281f6e974a530c78d15
Ariane Holford <aribomop@gmail.com>
2020-03-31 17:47:01 +0100

    fetch rates

--- src/app.js
+++ src/app.js
@@ -4,7 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: '#app',
         data: {
-            
+            exchangeRates: []
+        },
+        mounted(){
+            this.getRates()
+        },
+        methods: {
+            getRates: function(){
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(result => result.json())
+                .then(result => this.exchangeRates = result.rates)
+            }
         }
 
     })
