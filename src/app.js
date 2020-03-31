a951ebca7c4fb64bfd69048e82376a7f4393d23a
Ariane Holford <aribomop@gmail.com>
2020-03-31 18:40:22 +0100

    add euro to target currency conversion function

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: '#app',
         data: {
-            exchangeRates: []
+            exchangeRates: [],
+            selectedTargetRate: 0,
+            conversionAmount: 0,
         },
         mounted(){
             this.getRates()
+        },
+        computed: {
+            convertedCurrency: function(){
+                return (this.conversionAmount * this.selectedTargetRate).toFixed(2);
+             },
         },
         methods: {
             getRates: function(){
